b446a12a82cfc06aa71e19a3d2955ec054717fed
bobur_qalandar0v <qalandarovbobur274@gmail.com>
2025-07-16 14:06:16 +0500

    next-page

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,51 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const [showHeader, setShowHeader] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY < lastScrollY) {
+        setShowHeader(true);
+      } else {
+        setShowHeader(false);
+      }
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
   return (
     <>
-      <div className="header__background"></div>
-      <div className="container">
-        <header className="header">
-          <nav className="header__nav">
-            <Link className="header__logo" to="/">
-              <img src="/Logo.png" alt="logo" width={100} height={100} />
-            </Link>
-            <ul className="header__ul">
-              <li>
-                <Link className="header__link" to="/our-work">
-                  Proyektlar
-                </Link>
-              </li>
-              <li>
-                <Link className="header__link" to="/">
-                  Xizmat turlari
-                </Link>
-              </li>
-              <li>
-                <Link className="header__link" to="/">
-                  Biz haqimizda
-                </Link>
-              </li>
-              <li>
-                <Link className="header__link" to="/">
-                  Vakansiya / Buyurtma berish
-                </Link>
-              </li>
-              <li>
-                <Link className="header__link" to="/">
-                  Bizning jamoa
-                </Link>
-              </li>
-              <li>
-                <Link className="header__link" to="/">
-                  Kontaktlarimiz
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-      </div>
+      <header className={`header ${showHeader ? "visible" : "hidden"}`}>
+        <div className="container">
+          <div className="header__wrap">
+            <nav className="header__nav">
+              <Link className="header__logo" to="/">
+                <img src="/Logo.png" alt="logo" width={100} height={100} />
+              </Link>
+              <ul className="header__ul">
+                <li>
+                  <Link className="header__link" to="/our-work">
+                    Proyektlar
+                  </Link>
+                </li>
+                <li>
+                  <Link className="header__link" to="/">
+                    Xizmat turlari
+                  </Link>
+                </li>
+                <li>
+                  <Link className="header__link" to="/">
+                    Biz haqimizda
+                  </Link>
+                </li>
+                <li>
+                  <Link className="header__link" to="/">
+                    Vakansiya / Buyurtma berish
+                  </Link>
+                </li>
+                <li>
+                  <Link className="header__link" to="/">
+                    Bizning jamoa
+                  </Link>
+                </li>
+                <li>
+                  <Link className="header__link" to="/">
+                    Kontaktlarimiz
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
     </>
   );
 }
--- src/Pages/OurWorks/OurWorks.jsx
+++ src/Pages/OurWorks/OurWorks.jsx
@@ -1,11 +1,154 @@
 import React from "react";
+import QuotationIcon from "../../assets/Icons/QuotationIcon";
 
 function OurWorks() {
   return (
     <>
-      <div className="projects-page">
+      <div className="projects">
         <div className="container">
-            
+          <div className="projects-wrap">
+            <div className="projects-title">
+              <p className="title">Projects</p>
+              <p className="subtitle">
+                <span className="icon">
+                  <QuotationIcon fill="#001960" />
+                </span>
+                <span className="text">
+                  You love your home. We help make this love endless.
+                </span>
+              </p>
+            </div>
+            <div className="projects-list">
+              <div className="projects-list__item">
+                <div className="projects-list__img">
+                  <img src="/ProjectsImg/project-img.png" alt="project-img" />
+                </div>
+                <div className="projects-list__title">
+                  <h3 className="title">Coastal Luxury</h3>
+                </div>
+                <div className="projects-list__text">
+                  <p className="top">Cabo San Lucas, Mexico</p>
+                  <p className="bottom">
+                    21,928 sq.ft. 7 bed, 7 bath, 2 half-bath
+                  </p>
+                </div>
+              </div>
+              <div className="projects-list__item">
+                <div className="projects-list__img">
+                  <img
+                    src="/ProjectsImg/project-img.png"
+                    alt="project-img"
+                    width={440}
+                    height={250}
+                  />
+                </div>
+                <div className="projects-list__title">
+                  <h3 className="title">Coastal Luxury</h3>
+                </div>
+                <div className="projects-list__text">
+                  <p className="top">Cabo San Lucas, Mexico</p>
+                  <p className="bottom">
+                    21,928 sq.ft. 7 bed, 7 bath, 2 half-bath
+                  </p>
+                </div>
+              </div>
+              <div className="projects-list__item">
+                <div className="projects-list__img">
+                  <img
+                    src="/ProjectsImg/project-img.png"
+                    alt="project-img"
+                    width={440}
+                    height={250}
+                  />
+                </div>
+                <div className="projects-list__title">
+                  <h3 className="title">Coastal Luxury</h3>
+                </div>
+                <div className="projects-list__text">
+                  <p className="top">Cabo San Lucas, Mexico</p>
+                  <p className="bottom">
+                    21,928 sq.ft. 7 bed, 7 bath, 2 half-bath
+                  </p>
+                </div>
+              </div>
+              <div className="projects-list__item">
+                <div className="projects-list__img">
+                  <img
+                    src="/ProjectsImg/project-img.png"
+                    alt="project-img"
+                    width={440}
+                    height={250}
+                  />
+                </div>
+                <div className="projects-list__title">
+                  <h3 className="title">Coastal Luxury</h3>
+                </div>
+                <div className="projects-list__text">
+                  <p className="top">Cabo San Lucas, Mexico</p>
+                  <p className="bottom">
+                    21,928 sq.ft. 7 bed, 7 bath, 2 half-bath
+                  </p>
+                </div>
+              </div>
+              <div className="projects-list__item">
+                <div className="projects-list__img">
+                  <img
+                    src="/ProjectsImg/project-img.png"
+                    alt="project-img"
+                    width={440}
+                    height={250}
+                  />
+                </div>
+                <div className="projects-list__title">
+                  <h3 className="title">Coastal Luxury</h3>
+                </div>
+                <div className="projects-list__text">
+                  <p className="top">Cabo San Lucas, Mexico</p>
+                  <p className="bottom">
+                    21,928 sq.ft. 7 bed, 7 bath, 2 half-bath
+                  </p>
+                </div>
+              </div>
+              <div className="projects-list__item">
+                <div className="projects-list__img">
+                  <img
+                    src="/ProjectsImg/project-img.png"
+                    alt="project-img"
+                    width={440}
+                    height={250}
+                  />
+                </div>
+                <div className="projects-list__title">
+                  <h3 className="title">Coastal Luxury</h3>
+                </div>
+                <div className="projects-list__text">
+                  <p className="top">Cabo San Lucas, Mexico</p>
+                  <p className="bottom">
+                    21,928 sq.ft. 7 bed, 7 bath, 2 half-bath
+                  </p>
+                </div>
+              </div>
+              <div className="projects-list__item">
+                <div className="projects-list__img">
+                  <img
+                    src="/ProjectsImg/project-img.png"
+                    alt="project-img"
+                    width={440}
+                    height={250}
+                  />
+                </div>
+                <div className="projects-list__title">
+                  <h3 className="title">Coastal Luxury</h3>
+                </div>
+                <div className="projects-list__text">
+                  <p className="top">Cabo San Lucas, Mexico</p>
+                  <p className="bottom">
+                    21,928 sq.ft. 7 bed, 7 bath, 2 half-bath
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
